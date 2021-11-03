c5dedba8f89a6394d57afe66f5eaa407be1027c8
Juber8 <90597119+Juber8@users.noreply.github.com>
2021-11-03 14:26:34 +0530

    types of varible mentioned in file

---
+++ python varible.py
@@ -0,0 +1,41 @@
+
+'''class Juber:
+    pin=1245# class level static varible
+    def __init__(self,name,age,place):#init method
+        self.name=name  #instance varaible
+        self.age=age
+        self.place=place
+    def upadate(self,schoon_name):#instance method
+        self.school_name=schoon_name
+        print(self.school_name)
+s=Juber('juber',23,'patan')
+s.upadate('SGi')
+
+class Sample:
+    pin=1245 #class variable static varible
+    @staticmethod
+    def Upader(pin):
+        print
+s=Sample()
+s.Upader()
+Sample.Upader()
+
+class Car:
+    def Color(self):
+        print('color')
+class Vehicle(Car):
+    def Color(self):
+        print('wheel')
+class Sport_car(Car):
+    print('lamborgini')
+class Xuv_car(Car):
+    print('xuv700')
+
+s = Xuv_car()
+
+
+
+
+s=
+
+
